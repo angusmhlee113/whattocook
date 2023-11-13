c5b15c5c7b4d8794888b451d0da7931c8a0c1f3c
Angus Lee <angusmhlee@gmail.com>
2023-11-13 17:21:58 +0800

    Update envreader.go

--- envreader/envreader.go
+++ envreader/envreader.go
@@ -10,10 +10,9 @@ import (
 func ReadEnv() {
 	err := godotenv.Load()
 	if err != nil {
-		fmt.Println("")
+		fmt.Println("Cannot load .env file")
 	}
     database_url := os.Getenv("DATABASE_URL")
-
     fmt.Println(database_url)
 
 }
